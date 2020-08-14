2f512f749a2f3925ce3343ec59fe154811b6e377
Gabrielzeven <49731558+gbenselum@users.noreply.github.com>
2020-08-14 14:27:16 -0300

    Update fedora32.sh

--- fedora32.sh
+++ fedora32.sh
@@ -36,7 +36,7 @@ sudo dnf install vlc
 # vscodium
 sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg 
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
-sudo dnf install codium
+sudo dnf install codium -y
 
 
 
