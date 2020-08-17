bb0393a1ac52a24e610f516cfed2b66452a9ea1a
Gabrielzeven <49731558+gbenselum@users.noreply.github.com>
2020-08-17 00:54:00 -0300

    Update fedora32.sh

--- fedora32.sh
+++ fedora32.sh
@@ -24,7 +24,7 @@ sudo dnf install pavucontrol gnome-tweak-tool -y
 # media
 # obs studio 
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf install obs-studio -y
+sudo dnf install obs-studio audacity gimp -y
 
 # vlc
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
